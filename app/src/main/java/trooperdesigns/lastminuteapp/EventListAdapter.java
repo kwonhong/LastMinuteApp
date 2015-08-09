bb13102b4b28e9aaf4ee4dff7b2d763c15c428da
james <james@Jamess-MacBook-Pro.local>
2015-08-09 10:57:31 -0400

    Last Minute Application Initialization

---
+++ app/src/main/java/trooperdesigns/lastminuteapp/EventListAdapter.java
@@ -0,0 +1,148 @@
+package trooperdesigns.lastminuteapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EventListAdapter extends ParseQueryAdapter implements Filterable {
+
+	public static ParseObject event;
+
+	@SuppressWarnings("unchecked")
+	public EventListAdapter(Context context) {
+		
+		// Use the QueryFactory to construct a PQA that will only show
+		// Todos marked as high-pri
+		super(context, new ParseQueryAdapter.QueryFactory<ParseObject>() {
+			public ParseQuery<ParseObject> create() {
+				ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Event");
+				// First try to find from the cache and only then go to network
+				//query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE); // or CACHE_ONLY
+				//query.whereEqualTo("highPri", true);
+				return query;
+			}
+		});
+	}
+
+	// Customize the layout by overriding getItemView
+	@Override
+	public View getItemView(ParseObject event, View v, ViewGroup parent) {
+		if (v == null) {
+			v = View.inflate(getContext(), R.layout.row, null);
+		}
+
+		super.getItemView(event, v, parent);
+
+		this.event = event;
+
+		// Add and download the image
+		ParseImageView eventImage = (ParseImageView) v.findViewById(R.id.icon);
+		eventImage.setFocusable(false);
+		ParseFile imageFile = event.getParseFile("image");
+		if (imageFile != null) {
+			eventImage.setParseFile(imageFile);
+			eventImage.loadInBackground();
+		}
+
+		// Add the title view
+		TextView titleTextView = (TextView) v.findViewById(R.id.eventTitle);
+		titleTextView.setFocusable(false);
+		titleTextView.setText(event.getString("title").toUpperCase());
+
+		// TextView for Location (using details for dummy)
+		TextView locationTextView = (TextView) v.findViewById(R.id.eventLocation);
+		locationTextView.setFocusable(false);
+		locationTextView.setText(event.getString("details").toUpperCase());
+
+		// TODO: Check that dates exist, otherwise parsing error
+
+		// get Date object and use for formatting
+		Date startDate = event.getDate("startTime");
+		Date endDate = event.getDate("endTime");
+
+		SimpleDateFormat dateFormat;
+
+		dateFormat = new SimpleDateFormat("MM");
+		String month = getMonthName(Integer.parseInt(dateFormat.format(startDate))).toUpperCase();
+
+		// textView for Date
+		dateFormat = new SimpleDateFormat(" dd yyyy");
+		TextView dateTextView = (TextView) v.findViewById(R.id.eventDate);
+		dateTextView.setFocusable(false);
+		dateTextView.setText(month + dateFormat.format(startDate));
+
+		// textView for start time
+		dateFormat = new SimpleDateFormat("hh:mma");
+		TextView timeTextView = (TextView) v.findViewById(R.id.eventTime);
+		timeTextView.setFocusable(false);
+		timeTextView.setText(dateFormat.format(startDate) + " - " + dateFormat.format(endDate));
+
+		return v;
+	}
+
+	String getMonthName(int num) {
+		String month = "wrong";
+		DateFormatSymbols dfs = new DateFormatSymbols();
+		String[] months = dfs.getMonths();
+		if (num >= 0 && num <= 11 ) {
+			month = months[num];
+		}
+		return month;
+	}
+
+	@Override
+	public Filter getFilter() {
+		// TODO Auto-generated method stub
+	    Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                //arrayListNames = (List<String>) results.values;
+                notifyDataSetChanged();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                FilterResults results = new FilterResults();
+                ArrayList<String> FilteredArrayNames = new ArrayList<String>();
+
+                // perform your search here using the searchConstraint String.
+
+                constraint = constraint.toString().toLowerCase();
+/*                for (int i = 0; i < mDatabaseOfNames.size(); i++) {
+                    String dataNames = mDatabaseOfNames.get(i);
+                    if (dataNames.toLowerCase().startsWith(constraint.toString()))  {
+                        FilteredArrayNames.add(dataNames);
+                    }
+                }*/
+
+                results.count = FilteredArrayNames.size();
+                results.values = FilteredArrayNames;
+                Log.e("VALUES", results.values.toString());
+
+                return results;
+            }
+        };
+
+        return filter;
+	}
+
+}
---
+++ app/src/main/java/trooperdesigns/lastminuteapp/EventsFragment.java
@@ -0,0 +1,123 @@
+package trooperdesigns.lastminuteapp;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+public class EventsFragment extends ListFragment {
+	/**
+	 * The fragment argument representing the section number for this fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+	public static int numEvents;
+
+	private EventListAdapter eventsAdapter;
+	private ListView listView;
+	private SwipeRefreshLayout swipeLayout;
+
+	public EventsFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.fragment_events, container,
+				false);
+
+		listView = (ListView) rootView.findViewById(android.R.id.list);
+
+		swipeLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_container);
+		swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				//load();
+				eventsAdapter.loadObjects();
+				swipeLayout.setRefreshing(false);
+			}
+		});
+
+		// setting custom adapter for listFragment
+		eventsAdapter = new EventListAdapter(getActivity());
+		listView.setAdapter(eventsAdapter);
+		
+		// initial load of events list contents
+		eventsAdapter.loadObjects();
+		getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+		return rootView;
+	}
+	
+//	private void load(){
+//		// TODO Auto-generated method stub
+//		//Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in);
+//		animation.setDuration(800);
+//		listView.startAnimation(animation);
+//		setNumEvents();
+//		animation = null;
+//	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		listView = getListView(); // since you extend ListFragment
+		
+		listView.setOnItemLongClickListener(new OnItemLongClickListener(){
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> list, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				ParseObject object = (ParseObject) list.getItemAtPosition(position);
+				Toast.makeText(getActivity(), "Long click on: " + position + ": " + object.getString("title"),
+						Toast.LENGTH_SHORT).show();
+				return true;
+			}
+			
+		});
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// retrieve theListView item
+		ParseObject object = (ParseObject) l.getItemAtPosition(position);
+		DrawerActivity containerActivity = (DrawerActivity) getActivity();
+		
+		final android.support.v4.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+		//Intent i = new Intent(getActivity(), EventDetailsActivity.class);
+
+
+		// put all event information necessary for full event page in bundle
+		Bundle bundle = new Bundle();
+		bundle.putString("objectId", object.getObjectId()); // objectId of event
+		bundle.putString("title", object.getString("title")); // title of event
+		bundle.putString("createdAt", object.getCreatedAt().toString()); // createdAt
+
+		ParseFile pf = object.getParseFile("image");
+		if(pf != null){
+			bundle.putString("image", object.getParseFile("image").getUrl()); // put parse image file in bundle
+		}
+
+		//i.putExtras(bundle);
+
+		//startActivity(i);
+
+
+		// do something
+		//Toast.makeText(getActivity(), object.getString("title"), Toast.LENGTH_SHORT).show();
+	}
+	
+}
